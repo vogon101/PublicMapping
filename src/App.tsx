00ad37c965a5b16d8c38412ddc30a05a33c7f702
Freddie Poser <freddie.poser@gmail.com>
2024-09-21 15:45:53 +0200

    Fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import PerSquareMetreMap from './Pages/PerSquareMetreMap'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
